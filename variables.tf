b346931d9671569a024af3fbc7c3f31da1669e18
Buzzy <bwinster2@outlook.com>
2024-12-19 13:54:45 +0100

    Typo

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "prefix" {
   description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
 }
 
-variable "environement" {
+variable "environment" {
   type        = string
   description = "(Optional) Environnement"
 }
